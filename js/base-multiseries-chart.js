439582b710702465d59b0358764bc259781f1f8c
Dan Catalano <dev@nwbt.co>
2017-03-18 16:00:42 -0600

    multiseries chart - axis and color
    complete!

--- js/base-multiseries-chart.js
+++ js/base-multiseries-chart.js
@@ -14,7 +14,7 @@ export class BaseMultiseriesChart extends BaseChart {
 
   multi_series(years) {
 
-    let line, parseDate, parseYear, thisYear, color, paths, lastYear;
+    let line, parseDate, parseYear, thisYear, paths, lastYear;
     let yAxis, yAxisMin, yAxisMax;
     let scales = {};
 
@@ -25,7 +25,7 @@ export class BaseMultiseriesChart extends BaseChart {
 
     lastYear = d3.max(years, year => {
       return parseYear(+year.year);
-    });
+    }).getUTCFullYear();
 
     yAxisMin = 0;
     yAxisMax = d3.max(years, (year) => {
@@ -43,8 +43,6 @@ export class BaseMultiseriesChart extends BaseChart {
       .y(year => yAxis(year.flow))
       .curve(d3.curveBasis);
 
-    color = d3.scaleOrdinal(d3.schemeCategory20);
-
     years.forEach(year => {
       scales["scale" + year.year] = d3.scaleTime()
         .domain([new Date(+year.year, 0, 1), new Date(+year.year, 11, 31)])
@@ -56,64 +54,31 @@ export class BaseMultiseriesChart extends BaseChart {
       .enter()
       .append('path');
 
-    paths.attr('stroke', (year, idx) => {
-      return color(idx)
-    })
+    paths.attr('class', 'line')
+      .attr('fill', 'none')
       .attr('d', year => {
         thisYear = "scale" + year.year;
         return line(year.dates);
       })
-      .attr('fill', 'none');
+      .attr('id', (year) => {
+        if(+(year.year) === lastYear) {
+          return ('lastyear');
+        }
+      });
 
-    // let new_years_day = parseYear('2015-01-01');
-    // let new_years_eve = parseYear('2015-12-31');
-    //
-    // let x = d3.scaleTime().range([0, this.width]);
-    // let y = d3.scaleLinear().range([this.height, 0]);
-    // let z = d3.scaleOrdinal(d3.schemeCategory10);
-    //
-    // d3.timeMonth.range(new_years_day, new_years_eve);
-    //
-    // x.domain([new_years_day, new_years_eve]);
-    //
-    // let max = d3.max(years, (year) => {
-    //   return d3.max(year.dates, (dates) => {
-    //     return dates.flow;
-    //   })
-    // });
-    //
-    // y.domain([0, max]);
-    //
-    // this.chart.append('g')
-    //   .attr('class', 'axis--x')
-    //   .attr('transform', `translate(0, ${this.height})`)
-    //   .call(d3.axisBottom(x));
-    //
-    // this.chart.append('g')
-    //     .attr('class', 'axis axis--y')
-    //     .call(d3.axisLeft(y).tickSize(-this.width))
-    //   .append('text')
-    //     .attr('transform', 'rotate(-90)')
-    //     .attr('y', 6)
-    //     .attr('dy', '0.71em')
-    //     .attr('fill', '#000')
-    //     .text('Cubic Feet Per Second, (ft³/s)');
-    //
-    // let line = d3.line()
-    //   .curve(d3.curveBasis)
-    //   .x((d) => { return x(parseYear(d.date)); })
-    //   .y((d) => { return y(d.flow); });
-    //
-    // years.forEach((year) => {
-    //
-    //   let theLine = line(year.dates);
-    //
-    //   this.chart.append('g')
-    //       .attr('class', 'year')
-    //     .append('path')
-    //       .attr('class', 'line')
-    //       .attr('d', theLine)
-    //       .style('stroke', z(year.year));
-    // });
+    this.chart.append('g')
+      .attr('class', 'axis--x')
+      .attr('transform', `translate(0, ${this.height})`)
+      .call(d3.axisBottom(scales.scale2017).tickFormat(d => d3.timeFormat("%b")(d))); // do not understand this at all
+
+    this.chart.append('g')
+        .attr('class', 'axis axis--y')
+        .call(d3.axisLeft(yAxis).tickSize(-this.width))
+      .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 6)
+        .attr('dy', '0.71em')
+        .attr('fill', '#000')
+        .text('Cubic Feet Per Second, (ft³/s)');
   }
 }
